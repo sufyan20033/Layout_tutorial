bf4c0d4b912182fff385253a86b7d5248db82d1f
sufyan20033 <sufyanlehri10@gmail.com>
2025-03-16 16:39:15 +0500

    Debug Banner
    Removing the Debug Banner from screen

--- layout_tutorial/lib/main.dart
+++ layout_tutorial/lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
     const String appTitle = 'Flutter layout demo';
     return MaterialApp(
       title: appTitle,
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
           appBar: AppBar(title: const Text(appTitle)),
           body: const SingleChildScrollView(
