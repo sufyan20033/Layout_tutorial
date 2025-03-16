d95010e277525c42f61813fbd6f16526f5d2af7f
sufyan20033 <sufyanlehri10@gmail.com>
2025-03-16 16:37:58 +0500

    Added buttonsection
    Added the button Section in the main class

--- layout_tutorial/lib/main.dart
+++ layout_tutorial/lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
               name: 'Oeschinen Lake Campground',
               location: 'Kandersteg, Switzerland',
             ),
+            ButtonSection(),
           ]))),
     );
   }
