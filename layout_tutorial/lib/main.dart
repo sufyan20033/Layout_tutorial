31d711b8de5f23d521bd939893f1b155d71cc44b
sufyan20033 <sufyanlehri10@gmail.com>
2025-03-16 16:21:43 +0500

    Adding title class
    Adding title class and updated the body

--- layout_tutorial/lib/main.dart
+++ layout_tutorial/lib/main.dart
@@ -11,10 +11,52 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: appTitle,
       home: Scaffold(
-        appBar: AppBar(title: const Text(appTitle)),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
+          appBar: AppBar(title: const Text(appTitle)),
+          body: const SingleChildScrollView(
+            child: Column(
+              children: [
+
+              ]
+              )                 
+              )
+            ),
+    );
+  }
+}
+
+class TitleSection extends StatelessWidget {
+  const TitleSection({super.key, required this.name, required this.location});
+
+  final String name;
+  final String location;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /*1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /*2*/
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    name,
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Text(location, style: TextStyle(color: Colors.grey[500])),
+              ],
+            ),
+          ),
+          /*3*/
+          Icon(Icons.star, color: Colors.red[500]),
+          const Text('41'),
+        ],
       ),
     );
   }
